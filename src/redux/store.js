ce441ae797539bbb74e811851c48eb193db69465
Dariusz Wilk <dariusz.wilk.1993@gmail.com>
2023-09-27 18:51:16 +0100

    read data from store and use them in app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,7 @@
 import { createStore } from 'redux';
+import initialState from './initialState';
 const reducer = (state, action) => {
 	return state;
-};
-
-const initialState = {
-	columns: [],
 };
 
 const store = createStore(
